96189a3a1173ba853fe30213b9cc566eacdebac4
Ashish Rao <ashishrao2598@gmail.com>
2019-08-26 22:53:03 +0530

    add hgetall helper to main

--- album/main.go
+++ album/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gomodule/redigo/redis"
 )
+
+// Album struct describes the album structure
+type Album struct {
+	Title  string
+	Artist string
+	Price  float64
+	Likes  int
+}
 
 func main() {
 	conn, err := redis.Dial("tcp", ":6379")
@@ -20,6 +29,11 @@ func main() {
 	}
 	// calling the hGet()
 	err = hGet(conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = hgetAll(conn)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -53,4 +67,38 @@ func hGet(c redis.Conn) error {
 	}
 	fmt.Printf("%s by %s: £%.2f [%d likes]\n", title, artist, price, likes)
 	return nil
+}
+
+func hgetAll(c redis.Conn) error {
+	reply, err := redis.StringMap(c.Do("HGETALL", "album:2"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Use the populateAlbum helper function to create a new Album
+	// object from the map[string]string.
+	album, err := populateAlbum(reply)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", album)
+	return nil
+}
+
+// Create, populate and return a pointer to a new Album struct, based
+// on data from a map[string]string
+func populateAlbum(reply map[string]string) (*Album, error) {
+	var err error
+	album := new(Album)
+	album.Title = reply["title"]
+	album.Artist = reply["artist"]
+
+	album.Price, err = strconv.ParseFloat(reply["price"], 64)
+	if err != nil {
+		return nil, err
+	}
+	album.Likes, err = strconv.Atoi(reply["likes"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return album, nil
 }
